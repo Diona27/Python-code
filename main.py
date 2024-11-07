cfdfb2a5a9b71762936961da60c50e42ff22a6a0
Diona27 <906782@ssis.asia>
2024-11-07 10:20:29 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,33 @@
+#print ("please enter your full name")
+#MyName = input("Enter your full name: ")
+#len(MyName)
+#print (MyName [0:3])
+#print (MyName.upper())
+#print (MyName.lower())
+
+
+
+
+Num1 = input("Enter Number 1:")
+Num2 = input("Enter Number 2:")
+Num1 = int(Num1)
+Num2 = int(Num2)
+Ans1 = print("Sum of the numbers is:", Num1 + Num2)
+Ans2 = print("Difference of the numbers is:", Num1 - Num2)
+Ans3 = print("Product of the numbers is:", Num1 * Num2)
+Ans4 = print("Division of the numbers is:", Num1 / Num2)
+Ans5 = print("Number 1 raised to number 2 is:", Num1 ^ Num2)
+
+if Num1 > Num2:
+  print(Num1, "is bigger than", Num2)
+elif Num1 < Num2:
+  print(Num1, "is smaller than", Num2)
+else:
+  print(Num1, "is equal to", Num2)
+
+             
+#ans6 = print (Num3 * (Num1 + Num2))
+#ans7 = print (Num3 * (Num1 - Num2))
+#ans8 = print ((Num1 + Num2) ^ Num3)
+
+
